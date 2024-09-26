5a0430d24b17045ff772fcc0377952e9205516b3
razan <rzan17897@gmail.com>
2024-09-26 17:06:46 +0300

    Initial Commit

---
+++ water tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  water tracker
+//
+//  Created by razan alkhider on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ water tracker/water_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_trackerApp.swift
+//  water tracker
+//
+//  Created by razan alkhider on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
